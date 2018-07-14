3abbdd739531ffbb530ea60e9dfa05d4840958bc
Siwat Pruksapanya <siwat.pru@outlook.com>
2018-07-14 13:41:21 +0700

    :sparkles: Send sensor data to socket.io server

--- App.js
+++ App.js
@@ -1,13 +1,88 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import {Accelerometer, Gyroscope} from 'react-native-sensors';
+import DeviceInfo from 'react-native-device-info';
+import io from 'socket.io-client';
+
+const INTERVAL = 10;
 
 export default class App extends React.Component {
+  state = {name: DeviceInfo.getModel(), recordState: 'idle'};
+  accelerationObservable = null;
+  gyroscopeObservable = null;
+  lastAccelTime = 0;
+  lastGypoTime = 0;
+  socket = null;
+
+  onStopButtonPress() {
+    socket.disconnect()
+    accelerationObservable.stop()
+    gyroscopeObservable.stop()
+    this.setState({recordState: 'idle'})
+  }
+
+  onStartButtonPress() {
+    this.setState({recordState: 'recording'})
+    socket = io('http://192.168.180.200:3000');
+    socket.on('connect', () => {
+      console.log('connected to socket.io server');
+      socket.emit('config', {name: this.state.name})
+    });
+
+    new Accelerometer({updateInterval: INTERVAL})
+      .then(observable => {
+        accelerationObservable = observable;
+        accelerationObservable.subscribe(data => {
+          const {timestamp, x, y, z} = data;
+          const {name} = this.state;
+          if (timestamp > this.lastAccelTime) {
+            socket.emit('data', {type: 'accel', timestamp, x, y, z});
+            this.lastAccelTime = timestamp;
+          }
+        });
+      })
+      .catch(error => {
+        console.log('The Accelerometer is not available');
+      });
+    new Gyroscope({updateInterval: INTERVAL})
+      .then(observable => {
+        gyroscopeObservable = observable;
+        gyroscopeObservable.subscribe(data => {
+          const {timestamp, x, y, z} = data;
+          const {name} = this.state;
+          if (timestamp > this.lastGypoTime) {
+            socket.emit('data', {type: 'gyro', timestamp, x, y, z});
+            this.lastGypoTime = timestamp;
+          }
+        });
+      })
+      .catch(error => {
+        console.log('The Gyroscope is not available');
+      });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <TextInput
+          style={{height: 100}}
+          value={this.state.name}
+          onChangeText={name => this.setState({name})}
+        />
+        <Text>{this.state.recordState}</Text>
+        {this.state.recordState == 'idle' ? (
+          <Button
+            onPress={this.onStartButtonPress.bind(this)}
+            title="start"
+            color="#841584"
+          />
+        ) : (
+          <Button
+            onPress={this.onStopButtonPress.bind(this)}
+            title="Stop"
+            color="#841584"
+          />
+        )}
       </View>
     );
   }
